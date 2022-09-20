ff6945fc393d16e64ec9cfa313ea9121c2efd163
Lennon Martins <lennonmcarlos@gmail.com>
2022-09-20 18:11:21 -0400

    estilização com bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,77 @@
 <script>
-import axios from 'axios';
-export default {
-  name: "ListaDePokemons",
-  data() {
-    return { pokemons: [] };
-  },
-  async created() {
-    try {
-      let resposta = await axios.get('http://localhost:8080/api/v1/pokemons');
-      this.pokemons = resposta.data;
-    } catch (erro) {
-      console.log(erro);
-    }
-  }
-}
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-  <div>
-    <h2>Lista de Pokemons</h2>
-    <ul>
-      <li v-for="pokemon in pokemons" :key="pokemon.id">
-        <img :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + pokemon.numeroPokedex + '.png'" />
-        {{pokemon.numeroPokedex}} - {{pokemon.nome}}
-      </li>
-    </ul>
-  </div>
+  <header>
+    <nav class="stick-top navbar navbar-dark navbar-expand-lg bg-dark">
+      <div class="container-md">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">PokeDigix</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <RouterLink to="/" class="nav-link active" aria-current="page">Home </RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink to="/pokemons-selvagens" class="nav-link">Pokemons Selvagens</RouterLink>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  Ataques
+                </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <RouterLink class="dropdown-item" to="/ataques/novo">Cadastrar</RouterLink>
+                  </li>
+                  <li>
+                    <RouterLink class="dropdown-item" to="/ataques/lista">Lista</RouterLink>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  Tipos
+                </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <RouterLink class="dropdown-item" to="/tipos/novo">Cadastrar</RouterLink>
+                  </li>
+                  <li>
+                    <RouterLink class="dropdown-item" to="/tipos/lista">Lista</RouterLink>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  Pokemons
+                </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <RouterLink class="dropdown-item" to="/pokemons/novo">Cadastrar</RouterLink>
+                  </li>
+                  <li>
+                    <RouterLink class="dropdown-item" to="/pokemons/lista">Lista</RouterLink>
+                  </li>
+                </ul>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <main class="container">
+    <RouterView />
+  </main>
 </template>
 
-<style>
-
-</style>
---
+++ src/services/PokemonDataService.js
@@ -0,0 +1,44 @@
+import http from '../http-commons';
+
+class PokemonDataService {
+    async buscarTodos(){
+        let resposta = await http.get('/pokemons');
+        return resposta.data;
+    }
+
+    async buscarPeloId(id){
+        let resposta = await http.get('/pokemons/${id}');
+        return resposta.data;
+    }
+
+    async buscarPeloNome(nome){
+        let resposta = await http.get('/pokemons?termo=${nome}');
+        return resposta.data;
+    }
+
+    async buscarPeloIdDoTipo(id){
+        let resposta = await http.get('/pokemons/tipo/${id}');
+        return resposta.data;
+    }
+
+    async criar(pokemon){
+        let resposta = await http.post('/pokemons', pokemon)
+        return resposta.data;
+    }
+
+    async atualizar(id, pokemon){
+        let resposta = await http.put('/pokemons/${id}', pokemon);
+        return resposta.data;
+    }
+
+    async remover(id){
+        await http.delete('/pokemons/${id}');
+    }
+
+    async removerPeloNome(nome){
+        await http.delete('/pokemons/${nome}');
+    }
+
+}
+
+export default new PokemonDataService();
